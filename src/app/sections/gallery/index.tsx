3b19c8c461fc9abf99476ba2c3b242c06cf4848f
binhdoitsme <binhdoitsme@gmail.com>
2024-10-06 22:15:22 +0700

    feat: update gallery image frames

--- src/app/sections/gallery/index.tsx
+++ src/app/sections/gallery/index.tsx
@@ -51,18 +51,18 @@ export function Gallery() {
   }, [screenSize]);
 
   const showImages = useCallback(async () => {
-    animate(
+    await animate(
       scope.current.children,
-      { scale: 1, opacity: 1 },
-      { duration: 0.5, delay: stagger(0.1), ease: "circInOut" }
+      { y: 0, scale: 1, opacity: 1 },
+      { duration: 0.5, delay: stagger(0.1) }
     );
   }, []);
 
   const hideImages = useCallback(async () => {
-    animate(
+    await animate(
       scope.current.children,
-      { scale: 0, opacity: 0 },
-      { duration: 0.5, delay: stagger(0.1), ease: "circInOut" }
+      { y: 20, scale: 0, opacity: 0 },
+      { duration: 0.5, delay: stagger(0.1) }
     );
   }, []);
 
@@ -77,27 +77,36 @@ export function Gallery() {
   return (
     <div
       ref={ref}
-      className="w-full h-full border border-collapse flex flex-col items-center text-yellow-400 pt-4 pb-6 lg:pb-4"
+      className="w-full h-full border-y border-collapse flex flex-col items-center text-yellow-400 pt-4 pb-6 lg:pb-4"
     >
       <UnderlinedHeading text="Ảnh cưới" />
       {/* masonry layout */}
       <ScrollShadow>
         <div className="py-4 px-4 lg:px-16">
-          <div ref={scope} className="grid grid-cols-2 lg:grid-cols-5 gap-4">
+          <div
+            ref={scope}
+            className="grid grid-cols-2 lg:grid-cols-5 gap-4 lg:gap-y-8 h-auto"
+          >
             {images.map((src, index) => (
               <Card
                 key={index}
-                isFooterBlurred
                 radius="sm"
-                className="border-none"
+                className="border-none bg-slate-200 p-0.5"
               >
-                <Image
-                  alt="wedding photo"
-                  className="object-cover hover:scale-110 transition-transform"
-                  height={200}
-                  src={src}
-                  width={250}
-                />
+                <Card
+                  key={index}
+                  isFooterBlurred
+                  radius="sm"
+                  className="border border-slate-200"
+                >
+                  <Image
+                    alt="wedding photo"
+                    className="object-cover hover:scale-110 transition-transform"
+                    height={200}
+                    src={src}
+                    width={210}
+                  />
+                </Card>
               </Card>
             ))}
           </div>
